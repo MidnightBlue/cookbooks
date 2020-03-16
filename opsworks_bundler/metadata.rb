ec68a69a19b51369667fe2d3c963f36218f5a45f
MidnightBlue <isfore@gmail.com>
2020-03-16 15:01:46 +0800

    test

--- opsworks_bundler/metadata.rb
+++ opsworks_bundler/metadata.rb
@@ -1,5 +1,5 @@
-name        "opsworks_bundler2"
+name        "opsworks_bundler"
 description "Installs Bundler"
 maintainer  "AWS OpsWorks"
 license     "Apache 2.0"
-version     "1.0.0"
+version     "1.0.1"

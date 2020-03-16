426637a39f2bbf4982d4afc4de13e3362d4788c4
MidnightBlue <isfore@gmail.com>
2020-03-16 15:08:45 +0800

    test

--- opsworks_bundler/recipes/default.rb
+++ opsworks_bundler/recipes/default.rb
@@ -1,14 +1,14 @@
 if node[:opsworks_bundler][:manage_package]
-  gem_package "Installing Bundler #{node[:opsworks_bundler][:version]}" do
-    gem_binary node[:dependencies][:gem_binary]
-    retries 2
-    package_name "bundler"
-    action :install
-    version node[:opsworks_bundler][:version]
-    if Gem::Version.new(node[:opsworks_rubygems][:version]) > Gem::Version.new("2.6.14")
-      options "--force"
-    end
-  end
+  # gem_package "Installing Bundler #{node[:opsworks_bundler][:version]}" do
+  #   gem_binary node[:dependencies][:gem_binary]
+  #   retries 2
+  #   package_name "bundler"
+  #   action :install
+  #   version node[:opsworks_bundler][:version]
+  #   if Gem::Version.new(node[:opsworks_rubygems][:version]) > Gem::Version.new("2.6.14")
+  #     options "--force"
+  #   end
+  # end
 
   # alternative/fallback install of bundler for more robustness
   # handle cases where the gem library is there but the executable is missing
